8135ff73fe7592c8cf89553305f3eaff12223d7e
Matthew O'Donnell <matthew.l.odonnell@duke.edu>
2025-04-26 08:29:58 -0400

    Pusing first written dataset

--- FileOperations.py
+++ FileOperations.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from dateutil.relativedelta import relativedelta
+from tqdm import tqdm
+
 
 def ReadPTXT(year):
     Pcols = [
@@ -37,12 +40,12 @@ def ReadOTXT(year):
 def WriteDistressDataset(year):
     P = ReadPTXT(year)
 
-    # Create Stress Flags
+    # Create Stress Flag
     P.loc[(~P["Current Loan Delinquency Status"].isin(('0','1','2','3','4','5'))),"Major Stress"] = 1
 
     # Create ZB Flags
     P.loc[(P["Zero Balance Code"].isin((2.0, 3.0, 9.0))),"Zero Balance Default"] = 1
-    ZB1 = set(P[P["Zero Balance Code"] == 1.0]["Loan Sequence Number"].dropna().unique())
+    ZB1 = set(P[P["Zero Balance Code"] == 1.0]["Loan Sequence Number"].unique())
     P.loc[(P["Loan Sequence Number"].isin(ZB1)),"Ends In Payoff"] = 1
 
     # Find Invalid Defaults
@@ -56,39 +59,93 @@ def WriteDistressDataset(year):
     # Get Default Date for Defautled Mortgages
     P.loc[((P["Major Stress"]==1) | (P["Zero Balance Default"]==1)) & (~(P["Invalid Default"] == 1)), "Default Flag"] = 1
     
-    DefaultDate = P[P["Default Flag"]==1][["Loan Sequence Number","Monthly Reporting Period"]]
+    DefaultDate = P[P["Default Flag"]==1][["Loan Sequence Number","Monthly Reporting Period", 
+                                           "Actual Loss Calculation", "Zero Balance Removal UPB", "Net Sales Proceeds", 
+                                           "Delinquent Accrued Interest", "Expenses", "MI Recoveries", "Non MI Recoveries"]]
     DefaultDate = DefaultDate.sort_values(by=['Loan Sequence Number', 'Monthly Reporting Period'])
     DefaultDate = DefaultDate.rename(columns={'Monthly Reporting Period':'Default Date'})
     DefaultDate = DefaultDate.groupby("Loan Sequence Number").first().reset_index()
 
     # Create Distress Dataset
+    P = P.drop(columns=["Actual Loss Calculation", "Zero Balance Removal UPB", "Net Sales Proceeds", "Delinquent Accrued Interest", 
+                        "Expenses", "MI Recoveries", "Non MI Recoveries"])
     P = P.merge(DefaultDate, on="Loan Sequence Number", how="left")
+    P = P.rename(columns={'Monthly Reporting Period':'Distress Date'})
     Distress = P[(P["Major Stress"] == 1) | (P["Zero Balance Default"] == 1)]
-    DistressColumns = ["Loan Sequence Number", "Monthly Reporting Period", "Major Stress", "Default Flag",
-                       "Zero Balance Code", "Unresolved Delinquency", "Actual Loss Calculation", "Zero Balance Removal UPB", 
-                       "Net Sales Proceeds", "Delinquent Accrued Interest", "Expenses", "MI Recoveries", "Non MI Recoveries", "Most Recent Currency"]
+    DistressColumns = ["Loan Sequence Number", "Distress Date", "Major Stress", "Default Flag", "Default Date",
+                       "Zero Balance Code", "Unresolved Delinquency", "Most Recent Currency", 
+                       "Actual Loss Calculation", "Zero Balance Removal UPB", "Net Sales Proceeds", "Delinquent Accrued Interest", 
+                       "Expenses", "MI Recoveries", "Non MI Recoveries"
+                       ]
     
     Distress[DistressColumns].to_parquet(f"DefaultData/Defaults{year}.parquet", engine="pyarrow", index=False)
 
 def WriteTrainingData(year):
     
-    PcolsToDrop = [
+    PColsToDrop = [
         "Defect Settlement Date", "Zero Balance Code", "Zero Balance Effective Date", "Due Date of Last Paid Installment (DDLPI)",
         "MI Recoveries", "Net Sales Proceeds", "Non MI Recoveries", "Expenses", "Legal Costs", "Maintenance and Preservation Costs",
         "Taxes and Insurance", "Miscellaneous Expenses", "Actual Loss Calculation", "Modification Cost", 
         "Zero Balance Removal UPB", "Delinquent Accrued Interest", "Current Month Modification Cost"
         ]
     
-    OcolsToDrop = [
+    OColsToDrop = [
         "First Payment Date", "Maturity Date", "Channel", "Seller Name", "Servicer Name", "Super Conforming Flag", 
         "Pre-HARP Loan Sequence Number", "HARP Indicator"
         ]
     
-    P = ReadPTXT(year).drop(columns = PcolsToDrop)
-    O = ReadOTXT(year).drop(columns = OcolsToDrop)
+    P = ReadPTXT(year).drop(columns = PColsToDrop)
+    O = ReadOTXT(year).drop(columns = OColsToDrop)
     D = pd.read_parquet(f"DefaultData/Defaults{year}.parquet", engine="pyarrow")
 
+    print("Data Loaded!")
+
+    # Maximum Number of Months Each Loan Has Been PReviously Delinquent
+    P["Numeric Delinquency"] = pd.to_numeric(P["Current Loan Delinquency Status"], errors="coerce")
+    P = P.sort_values(["Loan Sequence Number", "Monthly Reporting Period"])
+    P["MaxPriorDelinquency"] = (P.groupby("Loan Sequence Number")["Numeric Delinquency"].expanding().max().shift().fillna(0).reset_index(level=0, drop=True))
+
+    P = P[P["Current Loan Delinquency Status"].isin(('0','1','2','3','4','5'))]
+    P = P[P["Monthly Reporting Period"].between(201101, 201812)]
+
     MERGED = O.merge(P, on="Loan Sequence Number", how="inner")
+
+    print("Merged Origination & Performance!")
+
+    DistressedLoans = set(D['Loan Sequence Number'].unique())
+    DistressDatePairs = set(zip(D['Loan Sequence Number'], D['Distress Date']))
+
+    def add_months(yyyymm, months):
+        year = yyyymm // 100
+        month = yyyymm % 100
+        NewDate = pd.Timestamp(year=year, month=month, day=1) + relativedelta(months=months)
+        return NewDate.year * 100 + NewDate.month
+
+    def find_first_match(row):
+        
+        ID = row['Loan Sequence Number']
+        if ID not in DistressedLoans:
+            return pd.NA
+        
+        Date = row['Monthly Reporting Period']
+        for i in range(12):
+            NextDate = add_months(Date, i)
+            if (ID, NextDate) in DistressDatePairs:
+                return NextDate
+        return pd.NA
+
+    tqdm.pandas(desc="Finding Dates Where Loans Are Within 12 Months Of Distress.")
+    MERGED["Distress Date"] = MERGED.progress_apply(find_first_match, axis=1)
+    MERGED = MERGED.merge(D, on=["Distress Date"], how='left')
+
+    print("Merged with Distress Data!")
+
+    for PerformanceYear in range(2011, 2018 + 1):
+        YearDF = MERGED[MERGED['Monthly Reporting Period'] // 100 == PerformanceYear]
+        FileName = f'TrainingData/Year{PerformanceYear}/TrainingData{PerformanceYear}Orig{year}.parquet'
+        YearDF.to_parquet(FileName, index=False, compression="snappy")
+
+        print(f"Wrote {FileName}!")
 
 
 
